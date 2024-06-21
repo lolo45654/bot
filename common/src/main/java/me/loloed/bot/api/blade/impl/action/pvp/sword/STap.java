17d4dc47b205bf8e9db81686cf77342fe8f60294
loloed <100manolo@web.de>
2024-06-21 11:40:31 +0200

    make jump and stap more likely

--- common/src/main/java/me/loloed/bot/api/blade/impl/action/pvp/sword/STap.java
+++ common/src/main/java/me/loloed/bot/api/blade/impl/action/pvp/sword/STap.java
@@ -3,9 +3,7 @@ package me.loloed.bot.api.blade.impl.action.pvp.sword;
 import me.loloed.bot.api.blade.impl.ConfigKeys;
 import me.loloed.bot.api.blade.planner.score.ScoreAction;
 import me.loloed.bot.api.blade.state.BladeState;
-import me.loloed.bot.api.util.BotMath;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -27,7 +25,7 @@ public class STap extends ScoreAction implements Sword {
     public double getScore() {
         LivingEntity target = bot.getBlade().get(ConfigKeys.TARGET);
         return getSwordScore(bot) +
-                target.hurtTime / 10.0 +
+                target.hurtTime / 6.0 +
                 ThreadLocalRandom.current().nextDouble() * 0.6;
     }
 }
