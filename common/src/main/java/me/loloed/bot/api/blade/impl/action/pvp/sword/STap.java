7a8f517672582a1a53afc16484c54daf7b490b57
loloed <100manolo@web.de>
2024-06-21 11:35:05 +0200

    remove dependency on distance for stap and jump

--- common/src/main/java/me/loloed/bot/api/blade/impl/action/pvp/sword/STap.java
+++ common/src/main/java/me/loloed/bot/api/blade/impl/action/pvp/sword/STap.java
@@ -26,12 +26,8 @@ public class STap extends ScoreAction implements Sword {
     @Override
     public double getScore() {
         LivingEntity target = bot.getBlade().get(ConfigKeys.TARGET);
-        Vec3 eyePos = bot.getVanillaPlayer().getEyePosition();
-        Vec3 closestPoint = BotMath.getClosestPoint(eyePos, target.getBoundingBox());
-        double distSq = closestPoint.distanceToSqr(eyePos);
         return getSwordScore(bot) +
                 target.hurtTime / 10.0 +
-                ThreadLocalRandom.current().nextDouble() * 0.6 +
-                (distSq <= 3 * 3 ? -8 : (Math.min(distSq / 3, 3)));
+                ThreadLocalRandom.current().nextDouble() * 0.6;
     }
 }
