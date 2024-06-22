7c87ff04d25ccfc0be97dd70d2ce831ec6dafef1
loloed <100manolo@web.de>
2024-06-22 11:58:23 +0200

    feat(kit bot): actually target

--- paper/src/main/java/me/loloed/bot/blade/KitBot.java
+++ paper/src/main/java/me/loloed/bot/blade/KitBot.java
@@ -2,6 +2,9 @@ package me.loloed.bot.blade;
 
 import com.mojang.authlib.GameProfile;
 import me.loloed.bot.api.Bot;
+import me.loloed.bot.api.blade.BladeMachine;
+import me.loloed.bot.api.blade.impl.ConfigKeys;
+import me.loloed.bot.api.blade.impl.goal.KillTargetGoal;
 import me.loloed.bot.api.impl.ServerBot;
 import me.loloed.bot.api.impl.ServerBotSettings;
 import me.loloed.bot.api.platform.Platform;
@@ -12,8 +15,10 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_20_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftLivingEntity;
 import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_20_R3.util.CraftLocation;
+import org.bukkit.entity.LivingEntity;
 
 import java.util.UUID;
 
@@ -24,8 +29,25 @@ public class KitBot extends ServerBot {
      *
      * @param pos     Position of the new bot.
      * @param spawner Player that is spawning the bot.
+     * @param target  Entity to start killing.
      * @return the newly spawned bot as a bukkit player
      */
+    public static org.bukkit.entity.Player create(Location pos, org.bukkit.entity.Player spawner, LivingEntity target) {
+        for (Bot bot : PaperPlatform.BOTS) {
+            if (bot instanceof ServerBot serverBot && serverBot.getSpawner().getUUID().equals(spawner.getUniqueId())) {
+                bot.destroy();
+            }
+        }
+        FakePlayer fakePlayer = new FakePlayer(BladePlugin.platform, MinecraftServer.getServer(), CraftLocation.toVec3D(pos), pos.getYaw(), pos.getPitch(), ((CraftWorld) pos.getWorld()).getHandle(), new GameProfile(UUID.randomUUID(), "KitBot"));
+        KitBot bot = new KitBot(fakePlayer, BladePlugin.platform, ((CraftPlayer) spawner).getHandle());
+        BladeMachine blade = bot.getBlade();
+        blade.set(ConfigKeys.TARGET, ((CraftLivingEntity) target).getHandle());
+        blade.setGoal(new KillTargetGoal());
+        BladePlugin.platform.addBot(bot);
+        return fakePlayer.getBukkitEntity();
+    }
+
+    @Deprecated(forRemoval = true)
     public static org.bukkit.entity.Player create(Location pos, org.bukkit.entity.Player spawner) {
         for (Bot bot : PaperPlatform.BOTS) {
             if (bot instanceof ServerBot serverBot && serverBot.getSpawner().getUUID().equals(spawner.getUniqueId())) {
