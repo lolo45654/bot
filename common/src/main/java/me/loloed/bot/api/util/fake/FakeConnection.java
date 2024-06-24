f39b9b0d2fa38a90cc2089581c001e604c0845ad
loloed <100manolo@web.de>
2024-06-24 14:58:20 +0200

    test kb changes p2

--- common/src/main/java/me/loloed/bot/api/util/fake/FakeConnection.java
+++ common/src/main/java/me/loloed/bot/api/util/fake/FakeConnection.java
@@ -43,10 +43,10 @@ public class FakeConnection extends Connection {
     @Override
     public void send(Packet<?> obj, @Nullable PacketSendListener packetSendListener, boolean bl) {
         if (obj instanceof ClientboundSetEntityMotionPacket) {
-            player.sentMotionPacket();
+            player.setDeltaMovement(player.hiddenDelta, false);
+            player.setDeltaMovement(Vec3.ZERO, true);
         } else if (obj instanceof ClientboundExplodePacket packet) {
-            player.sentMotionPacket();
-            // player.setDeltaMovement(player.getDeltaMovement(false).add(packet.getKnockbackX(), packet.getKnockbackY(), packet.getKnockbackZ()), false);
+            player.setDeltaMovement(player.hiddenDelta, false);
         }
     }
 
