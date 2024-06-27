a7314b04f6fc8cbef774bc429e7d5a91b26993be
loloed <100manolo@web.de>
2024-06-27 17:36:06 +0200

    fix(Strafe*): distance score

--- common/src/main/java/blade/impl/action/pvp/sword/StrafeRight.java
+++ common/src/main/java/blade/impl/action/pvp/sword/StrafeRight.java
@@ -32,7 +32,7 @@ public class StrafeRight extends ScoreAction implements Sword {
         double reach = getReach(bot);
 
         return getSwordScore(bot) +
-                (distSq <= reach * reach ? -8 : (Math.min(distSq, 6))) +
+                (distSq <= reach * reach ? -8 : (distSq > 12 * 12 ? -8 : Math.min(distSq / (12 * 12), 6))) +
                 Math.min(tick / 3.0, 1.4) +
                 ThreadLocalRandom.current().nextDouble() * 0.6;
     }
