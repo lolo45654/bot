10b0ec740e4796c8c3a7bc5055a6479c69e6957d
loloed <100manolo@web.de>
2024-07-11 16:19:50 +0200

    fix(util): AttackUtil#isAttacking dividing by too little

--- common/src/main/java/blade/impl/action/attack/ConsumeHealing.java
+++ common/src/main/java/blade/impl/action/attack/ConsumeHealing.java
@@ -68,7 +68,7 @@ public class ConsumeHealing extends BladeAction implements Attack {
         float targetHealthRatio = target.getHealth() / target.getMaxHealth();
 
         return 0 +
-                Math.min(distSq / (8 * 8) - 3 * 3, 1) +
+                Math.min((distSq - 3 * 3) / (8 * 8), 1) +
                 (tick > 0 ? 0.6 : 0) +
                 ((targetHealthRatio - ourHealthRatio) * 3 - bot.getBlade().get(ConfigKeys.DIFFICULTY)) +
                 (getHealingSlot() == null ? -12 : 0) +
